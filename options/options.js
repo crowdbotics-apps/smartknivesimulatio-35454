d4de6dec58a606db36fd415bea15881e94a83d2b
Crowdbotics <team@crowdbotics.com>
2022-05-26 14:59:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "smartknivesimulatio_35454",
+  url: "https://smartknivesimulatio_35454.botics.co",
+  api: "https://smartknivesimulatio_35454.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
